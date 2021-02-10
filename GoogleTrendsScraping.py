932cd2edb3ea8c3e07bf96bca2e9b7c74952f48e
hmhacks <henrymanley7@gmail.com>
2021-02-09 19:34:57 -0500

    trends abstraction

--- GoogleTrendsScraping.py
+++ GoogleTrendsScraping.py
@@ -5,9 +5,26 @@ Henry Manley - hjm67@cornell.edu -  Last Modified 2/8/2021
 """
 from pytrends.request import TrendReq
 import time
+import pandas as pd
 
+pytrends = TrendReq()
+
+
+# pytrend = TrendReq(hl='en-US', tz=360, retries=2, backoff_factor=0.1)
+# keywords = ['fires near me', 'smoke', 'air quality']
+#
+# pytrends.build_payload(
+#      kw_list=keywords,
+#      cat=0,
+#      timeframe='2019-09-01 2020-04-30',
+#      geo='AU-NSW',
+#      gprop='')
+#
+# data = pytrends.interest_over_time()
+# data= data.drop(labels=['isPartial'],axis='columns')
+# data.to_csv('./Data/Syd_Google_Smoke.csv', encoding='utf_8_sig')
 # Only need to run this once, the rest of requests will use the same session.
-# pytrends = TrendReq()
+#
 #
 # # Create payload and capture API tokens. Only needed for interest_over_time(), interest_by_region() & related_queries()
 # # pytrend.build_payload(kw_list=['unemployment', 'robinhood'])
@@ -21,6 +38,7 @@ import time
 #
 # # print(interest_over_time_df.head())
 # print(df.head())
+# stateList = ['NY', 'IL']
 
 stateList = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
           "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
@@ -50,7 +68,6 @@ def getGoogleTrends(termList, yearStart, yearEnd):
     months = 12
     googleLimit = 50000
     timeframe = months*(yearEnd - yearStart)
-    requests = len(termList)*timeframe
 
     accum = pd.DataFrame()
     iter = 0
@@ -60,16 +77,52 @@ def getGoogleTrends(termList, yearStart, yearEnd):
             stateQuery = 'US-' + stateList[y]
 
             if (iter + 1)* timeframe < googleLimit:
-                try:
-                    df = pytrends.get_historical_interest(termList[x], year_start=yearStart, year_end=yearEnd, geo=stateQuery, sleep=0)
-                except:
-                    pass
 
-                accum = accum.append(df)
+                data = makeRequest([termList[x]], yearStart, yearEnd, stateQuery)
+                data['State'] = stateList[y]
+                accum = accum.append(data)
+
                 iter += 1
 
             else:
                 time.sleep(90000)
-                iter = 0
+                data = makeRequest([termList[x]], yearStart, yearEnd, stateQuery)
+                data['State'] = stateList[y]
+                accum = accum.append(data)
+                iter = 1
 
-    accum.to_csv('./Data/GoogleTrends.csv', index=True)
+        filename = './Data/' + termList[x] + '.csv'
+        accum.to_csv(filename, index=True, encoding='utf_8_sig')
+
+
+def makeRequest(term, yearStart, yearEnd, stateQuery):
+    """
+    Builds payload and makes request to Google Trends API.
+    Returns request data.
+
+    Parameter term is the kw_list parameter being requested.
+    Preconditon term is a list with len 1 (one word at at time)
+
+    Parameter yearStart is the lower bound of the time series.
+    Preconditon: yearStart is an int
+
+    Parameter yearEnd is the upper bound of the time series.
+    Preconditon: yearEnd is an int
+
+    Parameter stateQuery is the state parameter being requested
+    Preconditon: stateQuery is a string of format "US-NY", eg.
+    """
+    pytrends.build_payload(
+         kw_list=term,
+         cat=0,
+         timeframe=  str(yearStart) + '-01-01 ' + str(yearEnd) + '-01-01',
+         geo=stateQuery,
+         gprop='')
+
+    data = pytrends.interest_over_time()
+    data= data.drop(labels=['isPartial'],axis='columns')
+
+    return data
+
+if __name__ == "__main__":
+    getGoogleTrends(['cornell'], 2015, 2019)
