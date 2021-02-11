592bd7a6d318141c089ab3889dfd2f5f1b980d30
hmhacks <henrymanley7@gmail.com>
2021-02-11 12:07:28 -0500

    working BLS @ state, non-farm level

--- Scripts/BLSScraping.py
+++ Scripts/BLSScraping.py
@@ -1,5 +1,5 @@
 """
-This script creates a state-industry level unemployment dataset.
+This script creates a state level unemployment dataset.
 
 Henry Manley - hjm67@cornell.edu -  Last Modified 2/8/2021
 """
@@ -7,18 +7,12 @@ import bls
 import pandas as pd
 import csv
 import time
-# #
-# inflation_and_prices1 = bls.get_series('SMU19197802023800001', 2004, 2013)
-# # 'SMU  19  19780 20238000 01'
-# # SMU40000000000000001
-# # SMU00000000000000001
-# #
+
+# Demo
+# inflation_and_prices1 = bls.get_series('SMU00000000000000001', 2004, 2013)
 # df = pd.DataFrame(inflation_and_prices1)
-# #
-# print(df)
-# #
+# print(df.head())
 # df.to_csv('../Data/ExtractedUnemployment.csv', index=True)
-#
 
 def getStateList():
     """
@@ -29,7 +23,6 @@ def getStateList():
     """
     with open('../Data/BLS/StateCodes.csv', newline='') as f:
         reader = csv.reader(f)
-
         accum = {}
         for row in reader:
             row = str(row)
@@ -39,9 +32,7 @@ def getStateList():
             accum[state] = code
 
     del accum['ate_code      state_name']
-
     return accum
-
 
 def getIndustryList():
     """
@@ -52,7 +43,6 @@ def getIndustryList():
     """
     with open('../Data/BLS/IndustryCodes.csv', newline='') as f:
         reader = csv.reader(f)
-
         accum = {}
         for row in reader:
             row = str(row)
@@ -62,38 +52,81 @@ def getIndustryList():
             accum[state] = code
 
     del accum['ode   industry_name']
-
     return accum
+
+def mapState(id):
+    """
+    Maps state ID number to two-letter state abbreviation.
+
+    Parameter id is the state ID.
+    Precondition: id is a str
+    """
+    id = str(id)
+
+    with open('../Data/BLS/StateCodes.csv', newline='') as f:
+        reader = csv.reader(f)
+        accum = []
+        for row in reader:
+            row = str(row)
+            state = row[10:-2]
+            code = row[2:4]
+            accum.append([state, code])
+    BLS_state_code = accum
+
+    with open('../Data/BLS/StateAbbrev.csv', newline='') as f:
+        reader = csv.reader(f)
+        accum = []
+        for row in reader:
+            row = str(row)
+            dash = row.index('-')
+            state = row[2:dash-1]
+            abbrev= row[dash + 2:-2]
+            accum.append([state, abbrev])
+    state_abbrev = accum
+
+    for pair in BLS_state_code:
+        if id == pair[1]:
+            STATE = pair[0]
+            for coup in state_abbrev:
+                if STATE == coup[0]:
+                    return coup[1]
+
 
 def unemployment():
     """
     State and Area Employment, Hours, and Earnings.
     Returns a csv with state-industry-level unemployment rates between 2004 and 2020.
     """
+    # Request Parameters
     state_list = list(getStateList().values())
-    # state_list = [state_list[40]]
-    industry_list = list(getIndustryList().values())
-    industry_list = [industry_list[0]]
+    state_list = state_list[1:-3]
 
-    accum = pd.DataFrame()
+    prefix = 'SM'
+    seasonal = 'U'
+    area = '00000'
+    industry = '00000000'
+    data_type = '01'
+
+    master = pd.DataFrame()
     iter = 0
 
     for state in state_list:
-        for industry in industry_list:
-            print(state)
-            series = "SMU" + state + "000000000000001"
-            print(series)
-
-            # Series Query
-            unemployment1 = bls.get_series(series, 2011, 2020)
-            df = pd.DataFrame(unemployment1)
-            accum = accum.append(df)
-            time.sleep(1)
-
         accum = pd.DataFrame()
-        accum.to_csv('../Data/BLS/' + series + '.csv', index=True)
+        print(state)
+        series = prefix + seasonal + state + area + industry + data_type
+        print(series)
 
+        # Series Query
+        unemployment1 = bls.get_series(series, 2011, 2020)
+        df = pd.DataFrame(unemployment1)
+        accum = accum.append(df)
+        accum['State'] = mapState(state)
 
+        master = master.append(accum)
+        accum = pd.DataFrame()
+        time.sleep(2)
+
+    master.to_csv('../Data/BLS/' + series + '.csv', index=True)
 
 
 if __name__ == "__main__":
